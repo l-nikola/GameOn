e847a7ba0b8fb3cb63f8375e1cc855a208bffa41
l-nikola <legay.nikola.pro@gmail.com>
2025-04-07 20:28:57 +0200

    fix : validation first name

--- script/modalValidation.js
+++ script/modalValidation.js
@@ -1,3 +1,20 @@
 function validate() {
-  alert("Validation successful!");
+  let isValid = true;
+
+  // First Name
+  const firstName = document.getElementById("first");
+  const firstNameError = firstName.parentElement;
+
+  if (firstName.value.trim().length < 2) {
+    firstNameError.setAttribute(
+      "data-error",
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+    firstNameError.setAttribute("data-error-visible", "true");
+    isValid = false;
+  } else {
+    firstNameError.setAttribute("data-error-visible", "false");
+  }
+
+  return isValid;
 }
