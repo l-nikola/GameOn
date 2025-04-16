b76b747cd7bd4de0a868911cd57c1f61e7a15aea
l-nikola <legay.nikola.pro@gmail.com>
2025-04-16 19:50:04 +0200

    fix : remove useless style and class + replace button style

--- script/modal.js
+++ script/modal.js
@@ -9,7 +9,7 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelectorAll(".btn-signup");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const closeConfirmationBtns = document.querySelectorAll(".close-confirmation");
